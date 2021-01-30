667d8b9aa4efaf700f54931efa0176875db86fad
Eduardo Flores <efloresambrosio@gmail.com>
2021-01-29 18:34:44 -0600

    new: initial source code

---
+++ src/main/java/com/fronzec/marvelcharacters/MarvelCharactersMsApplication.java
@@ -0,0 +1,13 @@
+package com.fronzec.marvelcharacters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvelCharactersMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarvelCharactersMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fronzec/marvelcharacters/MarvelCharactersMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fronzec.marvelcharacters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarvelCharactersMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
